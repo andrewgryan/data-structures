8904cb6c432c67325b33af51a9f39bf5c7c7d1ce
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-09-12 10:27:42 +0100

    add types

--- balanced-tree/lib.js
+++ balanced-tree/lib.js
@@ -1,4 +1,17 @@
-function Node(value) {
+// @ts-check
+
+/**
+ * @typedef {object} BinaryNode
+ * @property {number} value
+ * @property {BinaryNode | null} left
+ * @property {BinaryNode | null} right
+ */
+
+/**
+ *  @param {number} value
+ *  @returns {BinaryNode} new search tree
+ */
+function BinaryNode(value) {
   return {
     value,
     left: null,
@@ -6,6 +19,10 @@ function Node(value) {
   };
 }
 
+/**
+ *  @param {BinaryNode} node
+ *  @param {number[]} values
+ */
 function insertMany(node, values) {
   values.forEach((value) => {
     node = insert(node, value);
@@ -13,8 +30,13 @@ function insertMany(node, values) {
   return node;
 }
 
+/**
+ *  @param {BinaryNode | null} node
+ *  @param {number} value
+ *  @returns {BinaryNode} new search tree
+ */
 function insert(node, value) {
-  if (node === null) return Node(value);
+  if (node === null) return BinaryNode(value);
   if (value < node.value) {
     node.left = insert(node.left, value);
   } else if (value > node.value) {
@@ -23,52 +45,90 @@ function insert(node, value) {
   return node;
 }
 
+/**
+ *  @param {BinaryNode | null} tree
+ */
 function height(tree) {
   return _height(tree, 0);
 }
 
+/**
+ *  @param {BinaryNode | null} node
+ *  @param {number} edges
+ */
 function _height(node, edges) {
   if (node === null) return edges;
   if (isLeaf(node)) return edges;
   return Math.max(_height(node.left, edges), _height(node.right, edges)) + 1;
 }
 
+/**
+ *  @param {BinaryNode} node
+ */
 function isLeaf(node) {
   return node.left === null && node.right === null;
 }
 
+/**
+ *  @param {BinaryNode} node
+ */
 function balance_factor(node) {
   return height(node.left) - height(node.right);
 }
 
+/**
+ *  @param {BinaryNode} node
+ *  @returns {BinaryNode}
+ */
 function leftRotate(node) {
   let root = node;
   let pivot = node.right;
+  if (pivot === null) return root
   root.right = pivot.left;
   pivot.left = root;
   return pivot;
 }
 
+/**
+ *  @param {BinaryNode} node
+ *  @returns {BinaryNode}
+ */
 function rightRotate(node) {
   let root = node;
   let pivot = node.left;
+  if (pivot === null) return root
   root.left = pivot.right;
   pivot.right = root;
   return pivot;
 }
 
+/**
+ *  @param {BinaryNode} node
+ *  @returns {BinaryNode}
+ */
 function rightLeftRotate(node) {
   let child = node.right;
+  if (child === null) return node
   node.right = rightRotate(child);
   return leftRotate(node);
 }
 
+/**
+ *  @param {BinaryNode} node
+ *  @returns {BinaryNode}
+ */
 function leftRightRotate(node) {
   let child = node.left;
+  if (child === null) return node
   node.left = leftRotate(child);
   return rightRotate(node);
 }
 
+/**
+ *  @param {BinaryNode} node
+ *  @param {number} indent
+ *  @param {(n: BinaryNode) => number} fn
+ */
 function postOrder(node, indent, fn) {
   if (node !== null) {
     // Format
@@ -90,40 +150,54 @@ function postOrder(node, indent, fn) {
   }
 }
 
+/**
+ *  @param {BinaryNode} node
+ */
 function summary(node) {
   console.log({ height: height(node), balance_factor: balance_factor(node) });
   postOrder(node, 0, (n) => n.value);
   postOrder(node, 0, balance_factor);
 }
 
+/**
+ * @param {number} root
+ * @param {number[]} data
+ * @returns {BinaryNode} binary search tree
+ */
+function bst(root, ...data) {
+  return insertMany(BinaryNode(root), data)
+}
+
 /// Executable code
-let node = insertMany(Node(42), [41, 43, 44, 45, 46]);
+let node = bst(42, 41, 43, 44, 45, 46);
 
 summary(node);
 node = leftRotate(node);
 summary(node);
-node.right = leftRotate(node.right);
-summary(node);
+if (node.right !== null) {
+  node.right = leftRotate(node.right);
+  summary(node);
+}
 
 // Duplicate insert
-summary(insert(Node(42), 42));
+summary(bst(42, 42));
 
 // Right rotate test
-let right = insertMany(Node(5), [4, 3, 2, 1]);
+let right = bst(5, 4, 3, 2, 1);
 summary(right);
 right = rightRotate(right);
 summary(right);
 
 // Right left rotate scenario
 console.log("-- Right left rotate --");
-let rightLeft = insertMany(Node(10), [20, 19, 18]);
+let rightLeft = bst(10, 20, 19, 18);
 summary(rightLeft);
 rightLeft = rightLeftRotate(rightLeft);
 summary(rightLeft);
 
 // Left right rotate scenario
 console.log("-- Left right rotate --");
-let leftRight = insertMany(Node(30), [20, 21, 22]);
+let leftRight = bst(30, 20, 21, 22);
 summary(leftRight);
 leftRight = leftRightRotate(leftRight);
 summary(leftRight);
